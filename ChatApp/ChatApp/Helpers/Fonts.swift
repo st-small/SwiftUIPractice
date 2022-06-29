53d6e0e8507b6c3b866f079980829899610f1793
Stanislav Shiyanovskiy <shiyanovskiy.s@richpeach.pro>
2022-06-29 19:13:42 +0300

    ChatApp
    - rename body, caption and title font properties to not clash with existing ones

--- ChatApp/ChatApp/Helpers/Fonts.swift
+++ ChatApp/ChatApp/Helpers/Fonts.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 extension Font {
-    public static var body: Font {
+    public static var bodyParagraph: Font {
         return Font.custom("LexendDeca-Regualr", size: 14)
     }
     
@@ -16,7 +16,7 @@ extension Font {
         return Font.custom("LexendDeca-SemiBold", size: 14)
     }
     
-    public static var caption: Font {
+    public static var smallText: Font {
         return Font.custom("LexendDeca-Regular", size: 10)
     }
     
@@ -28,7 +28,7 @@ extension Font {
         return Font.custom("LexendDeca-Regular", size: 16)
     }
     
-    public static var title: Font {
+    public static var titleText: Font {
         return Font.custom("LexendDeca-Bold", size: 23)
     }
     
